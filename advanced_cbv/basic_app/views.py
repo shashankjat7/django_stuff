27dd8f656809f950076c79398089c4f43f012778
newt0n <shashankjat7@gmail.com>
2021-01-04 10:51:31 +0530

    CBV

---
+++ advanced_cbv/basic_app/views.py
@@ -0,0 +1,11 @@
+# class based views
+
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+
+# Create your views here.
+
+class CBView(View):
+    def get(self,request):
+        return HttpResponse('CBV are cool')
